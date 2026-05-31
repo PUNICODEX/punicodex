b1281239638b7eb16aeb0a42253b88729ed57105
Hekaverse <admin@hekaverse.com>
2026-06-01 07:15:48 +1000

    chore: remove temp audit scripts

--- tmp_check_domains.py
+++
@@ -1,7 +0,0 @@
-import re, sys
-sys.stdout.reconfigure(encoding='utf-8')
-with open('js/archetypes-v2.js', 'r', encoding='utf-8') as f:
-    content = f.read()
-pattern = r'id:\s*"([^"]+)",.*?domainUnicode:\s*"([^"]+)"'
-for m in re.finditer(pattern, content, re.DOTALL):
-    print(f'{m.group(1)} -> {m.group(2)}')
